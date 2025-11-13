b9e1df98bf5bcc133292c1e322e3c01726ce6fe2
Vinicius <vinicius.padua@tarvos.ag>
2025-11-13 19:59:56 -0300

    feat: implementing EnvConfig interface

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { EnvConfig } from './env-config.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+  constructor(private configService: ConfigService) {}
+  getAppPort(): number {
+    return Number(this.configService.get<number>('PORT'));
+  }
+
+  getNodeEnv(): string {
+    return this.configService.get<string>('NODE_ENV', 'development');
+  }
+
+  getJwtSecret(): string {
+    const secret = this.configService.get<string>('JWT_SECRET');
+
+    if (!secret) {
+      throw new Error('Missing environment variable: JWT_SECRET');
+    }
+
+    return secret;
+  }
+
+  getJwtExpiresInSeconds(): number {
+    return Number(this.configService.get<number>('JWT_EXPIRES_IN'));
+  }
+}
