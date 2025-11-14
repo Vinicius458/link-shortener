036986a181c07b790d22a3f1cf3b9b5d38d6c31d
Vinicius <vinicius.padua@tarvos.ag>
2025-11-14 16:10:54 -0300

    fix: class validator fields

--- src/shared/domain/validators/class-validator-fields.ts
+++ src/shared/domain/validators/class-validator-fields.ts
@@ -1,26 +1,26 @@
-import { validateSync } from 'class-validator'
+import { validateSync } from 'class-validator';
 import {
   FieldsErrors,
   ValidatorFieldsInterface,
-} from './validator-fields.interface'
+} from './validator-fields.interface';
 
 export abstract class ClassValidatorFields<PropsValidated>
   implements ValidatorFieldsInterface<PropsValidated>
 {
-  errors: FieldsErrors = null
-  validatedData: PropsValidated = null
+  errors: FieldsErrors = null;
+  validatedData: PropsValidated | null = null;
 
   validate(data: any): boolean {
-    const errors = validateSync(data)
+    const errors = validateSync(data);
     if (errors.length) {
-      this.errors = {}
+      this.errors = {};
       for (const error of errors) {
-        const field = error.property
-        this.errors[field] = Object.values(error.constraints)
+        const field = error.property;
+        this.errors[field] = Object.values(error.constraints ?? {});
       }
     } else {
-      this.validatedData = data
+      this.validatedData = data;
     }
-    return !errors.length
+    return !errors.length;
   }
 }
--- src/shared/domain/validators/validator-fields.interface.ts
+++ src/shared/domain/validators/validator-fields.interface.ts
@@ -1,9 +1,9 @@
 export type FieldsErrors = {
-  [field: string]: string[]
-}
+  [field: string]: string[];
+} | null;
 
 export interface ValidatorFieldsInterface<PropsValidated> {
-  errors: FieldsErrors
-  validatedData: PropsValidated
-  validate(data: any): boolean
+  errors: FieldsErrors;
+  validatedData: PropsValidated | null;
+  validate(data: any): boolean;
 }
