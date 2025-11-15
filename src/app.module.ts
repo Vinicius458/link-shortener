a05b217beb751e76679ffc060bcc80f0859c7424
Vinicius <vinicius.padua@tarvos.ag>
2025-11-15 12:00:20 -0300

    feat: nest configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 import { UsersModule } from './users/infrastructure/users.module';
 import { AuthModule } from './auth/infrastructure/auth.module';
@@ -8,7 +6,5 @@ import { DatabaseModule } from './shared/infrastructure/database/database.module
 
 @Module({
   imports: [EnvConfigModule, UsersModule, DatabaseModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
